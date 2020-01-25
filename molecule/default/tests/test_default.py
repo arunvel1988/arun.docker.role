3a2f8553522ce3708e4d8e8eee42ed2d8033314a
Your Name <you@example.com>
2020-01-25 05:42:41 +0530

    DAD

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,23 @@
+import os
+import pytest
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+
+@pytest.mark.parametrize("name", [
+    ("docker-ce"),
+])
+def test_packages(host, name):
+    pkg = host.package(name)
+    assert pkg.is_installed
